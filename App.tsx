76ac0b1872cb029218e232bb85eb7ff077de2611
preciousimo <preciousimoniakemu@gmail.com>
2022-11-26 17:20:18 +0100

    Install and setup apollo client

--- App.tsx
+++ App.tsx
@@ -1,9 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+const API_KEY = "josedefreitas::stepzen.net+1000::c206dd810d230c611cc8c1fa1f25b2c7e43a322ee225c5a1b704d2afd7446db4";
+
+const client = new ApolloClient({
+  uri: " https://josedefreitas.stepzen.net/api/funny-macaw/__graphql",
+  headers: {
+    Authorization: `Apikey ${API_KEY}`,
+  },
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,8 +25,9 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+       <ApolloProvider client={client}>
+          <Navigation colorScheme={colorScheme} />
+        </ApolloProvider>
       </SafeAreaProvider>
     );
   }
