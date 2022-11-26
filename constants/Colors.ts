018d510d517121f1dc96b3f6eb45f7cc5edb2164
preciousimo <preciousimoniakemu@gmail.com>
2022-11-26 16:44:17 +0100

    Initialize expo app

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,4 +1,4 @@
-const tintColorLight = '#2f95dc';
+const tintColorLight = '#46AA71';
 const tintColorDark = '#fff';
 
 export default {
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -4,12 +4,10 @@ import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text style={styles.title}>Hello Devs</Text>
     </View>
   );
 }
