bb245af0638e033bfa09c2a086ab8ef205e9b1e8
preciousimo <preciousimoniakemu@gmail.com>
2022-11-26 17:38:19 +0100

    Setup apollo client

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -3,8 +3,50 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { gql, useQuery } from '@apollo/client';
+
+
+
+const query = gql`
+  query SearchBooks($q: String) {
+    googleBooksSearch(q: $q, country: "US") {
+      items {
+        id
+        volumeInfo {
+          authors
+          averageRating
+          description
+          imageLinks {
+            thumbnail
+          }
+          title
+          subtitle
+          industryIdentifiers {
+            identifier
+            type
+          }
+        }
+      }
+    }
+    openLibrarySearch(q: $q) {
+      docs {
+        author_name
+        title
+        cover_edition_key
+        isbn
+      }
+    }
+  }
+`;
+
 
 export default function TabOneScreen() {
+  const { data, loading, error } = useQuery(query, { variables: { q: "Django" } });
+
+  console.log(data);
+  console.log(loading);
+  console.log(error);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hello Devs</Text>
