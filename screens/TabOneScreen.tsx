15ed65cb94c69ab62a0eb3362258ec2aa1e78415
preciousimo <preciousimoniakemu@gmail.com>
2022-11-27 16:32:25 +0100

    Create tabs for Google Books and Open Library

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { ActivityIndicator, StyleSheet, FlatList, TextInput, Button } from 'react-native'; 
+import { ActivityIndicator, StyleSheet, FlatList, TextInput, Button } from 'react-native';
 
-import { Text, View } from '../components/Themed'; 
+import { Text, View } from '../components/Themed';
 import { gql, useQuery, useLazyQuery } from '@apollo/client';
 import BookItem from '../components/BookItem';
 
@@ -41,7 +41,28 @@ const query = gql`
 
 export default function TabOneScreen() {
   const [search, setSearch] = useState("");
+  const [provider, setProvider] = useState<
+    "googleBooksSearch" | "openLibrarySearch"
+  >("googleBooksSearch");
+
   const [runQuery, { data, loading, error }] = useLazyQuery(query);
+
+  const parseBook = (item: any): Book => {
+    if (provider == "googleBooksSearch") {
+      return {
+        image: item.volumeInfo.imageLinks?.thumbnail,
+        title: item.volumeInfo.title,
+        authors: item.volumeInfo.authors,
+        isbn: item.volumeInfo.industryIdentifiers?.[0]?.identifier,
+      };
+    }
+    return {
+      image: `https://covers.openlibrary.org/b/olid/${item.cover_edition_key}-M.jpg`,
+      title: item.title,
+      authors: item.author_name,
+      isbn: item.isbn?.[0],
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -52,8 +73,21 @@ export default function TabOneScreen() {
           placeholder='Search...'
           style={styles.input}
         />
-        <Button title='Search' onPress={() => runQuery({ variables: {q: search}})}/>
+        <Button title='Search' onPress={() => runQuery({ variables: { q: search } })} />
       </View>
+
+      <View style={styles.tabs}>
+        <Text
+          style={provider == "googleBooksSearch" ? { fontWeight: 'bold', color: 'royalblue' } : {}}
+          onPress={() => setProvider("googleBooksSearch")}
+        >Google Books</Text>
+        <Text
+          style={provider == "openLibrarySearch" ? { fontWeight: 'bold', color: 'royalblue' } : {}}
+          onPress={() => setProvider("openLibrarySearch")}
+        >Open Library</Text>
+      </View>
+
+
       {loading && <ActivityIndicator />}
       {error && (
         <>
@@ -61,17 +95,15 @@ export default function TabOneScreen() {
           <Text>{error.message}</Text>
         </>
       )}
-      <FlatList 
-        data={data?.googleBooksSearch?.items || []}
+      <FlatList
+        data={
+          (provider == 'googleBooksSearch'
+            ? data?.googleBooksSearch?.items
+            : data?.openLibrarySearch?.docs) || []}
         showsVerticalScrollIndicator={false}
         renderItem={({ item }) => (
-          <BookItem 
-            book={{ 
-              image: item.volumeInfo.imageLinks?.thumbnail,
-              title: item.volumeInfo.title,
-              authors: item.volumeInfo.authors,
-              isbn: item.volumeInfo.industryIdentifiers?.[0]?.identifier,
-            }} 
+          <BookItem
+            book={parseBook(item)}
           />
         )}
       />
@@ -104,5 +136,11 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
     marginVertical: 5,
+  },
+  tabs: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    height: 50,
+    alignItems: "center",
   },
 });
