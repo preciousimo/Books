109a8702f1dd146aec92c351a1a5a4f84ba157f6
preciousimo <preciousimoniakemu@gmail.com>
2022-11-27 22:40:18 +0100

    Implement MyBooks Context

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,16 +1,24 @@
-import { StyleSheet } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
+import BookItem from '../components/BookItem';
 import { View } from '../components/Themed';
 import { useMyBooks } from '../context/MyBooksProvider';
 
 export default function TabTwoScreen() {
-  return <View style={styles.container}></View>
+  const { savedBooks } = useMyBooks();
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={savedBooks}
+        renderItem={({ item }) => <BookItem book={item} />}
+      />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
   },
   title: {
     fontSize: 20,
